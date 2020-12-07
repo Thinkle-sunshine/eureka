058e15b8d6c874ae516a7a2cb7f89eda0ab76c3e
wangls <thinkle0328@gmali.com>
2020-12-07 15:43:32 +0800

    删除test项目，添加mongodb项目

---
+++ mongo/src/test/java/com/wls/MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
